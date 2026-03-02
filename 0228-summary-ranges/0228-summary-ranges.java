9cce2eca6773971d0e015a1277a8659f9215c9db
Mudit <40465742+Mudit-Nahata@users.noreply.github.com>
2026-03-02 02:10:02 -0700

    Time: 3 ms (78.9%), Space: 43.1 MB (72.35%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> output = new ArrayList<String>();
+        if (nums.length == 0) return output;
+        int i = 0;
+        while(i < nums.length) {
+            int start = nums[i];
+            int j = i;
+            while(j + 1 < nums.length && nums[j + 1] == nums[j] + 1) j++;
+            if (nums[j] == start) output.add(String.valueOf(start));
+            else output.add(start+"->"+nums[j]);
+            i = j + 1;
+        }
+        return output;
+    }
+}
