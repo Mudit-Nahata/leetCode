d8f7290456a93aa9b16b5a5f3e9abe6c403bfc7c
Mudit <40465742+Mudit-Nahata@users.noreply.github.com>
2026-03-03 13:08:52 +0530

    Time: 3 ms (21.42%), Space: 47.4 MB (47.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,6 +1,8 @@
 import java.math.BigInteger;
 class Solution {
     public int missingNumber(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        int org = ( nums.length * ( nums.length + 1 ) ) / 2;
 
         return ( nums.length * ( nums.length + 1 ) ) / 2 - Arrays.stream(nums).sum();
     }
