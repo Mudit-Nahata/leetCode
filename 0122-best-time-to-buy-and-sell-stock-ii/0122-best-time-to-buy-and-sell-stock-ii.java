a2c5cd26fc08b30ad985f0209384d01f6f817c89
Mudit <40465742+Mudit-Nahata@users.noreply.github.com>
2026-04-14 15:07:03 +0530

    Time: 0 ms (100%), Space: 46.5 MB (33.92%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,17 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0; int first = prices[0]; int total = 0;
-        for(int i = 1; i < prices.length; i++){
-            if(profit > prices[i]- first) {
-                first = prices[i];
-            } else {
-                profit = prices[i] - first;
-                total = total + profit;
-                first = prices[i];
-                profit = 0;
+        int profit =0;
+
+        for(int i =1; i< prices.length; i++){
+
+            if(prices[i]> prices[i-1]){
+                profit += prices[i]- prices[i-1];
             }
-            
         }
-        return total;
+        return profit;
+        
     }
 }
+ 
