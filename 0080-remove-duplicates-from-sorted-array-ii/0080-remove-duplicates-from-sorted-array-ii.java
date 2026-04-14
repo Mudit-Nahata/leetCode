709d8d6db619646ffbe06316d95f820f8a6455e8
Mudit <40465742+Mudit-Nahata@users.noreply.github.com>
2026-04-14 12:43:17 +0530

    Time: 0 ms (100%), Space: 48.6 MB (85.74%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,26 +1,22 @@
 class Solution {
-    
     public int removeDuplicates(int[] nums) {
-        int count = 0;
-        int k = 0;
-        int currentValue = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if(nums[i] == nums[i-1]) {
-                count++;
-            }
-            if (currentValue != nums[i] || i == nums.length - 1) {
-                while(count > 1) {
-                    nums[i-count] = Integer.MAX_VALUE;
-                    count--;
-                    k++;
+        int nowNum = 10000000;
+        int numCount = 0;
+        int idx = 0;
+        
+        for(int num : nums) {
+            if (num != nowNum) {
+                nums[idx++] = num;
+                nowNum = num;
+                numCount = 1;
+            } else {
+                if (numCount <= 1) {
+                    nums[idx++] = num;
+                    numCount++;
                 }
-                count = 0;
-                currentValue = nums[i];
             }
-            
         }
-        Arrays.sort(nums);
-        return nums.length - k;
+
+        return idx;
     }
-    
 }
