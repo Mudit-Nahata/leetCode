37d407b0a7c094c80784842f758ac9941bdc3121
Mudit <40465742+Mudit-Nahata@users.noreply.github.com>
2026-04-25 14:00:04 +0530

    Time: 29 ms (35.13%), Space: 100.5 MB (64.63%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1-sol2.java
@@ -0,0 +1,36 @@
+class RandomizedSet {
+    private Map<Integer, Integer> map;  // val -> index
+    private List<Integer> list;         // stores values
+    private Random rand;
+    
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        rand = new Random();
+    }
+    
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        
+        int index = map.get(val);
+        int lastVal = list.get(list.size() - 1);
+        
+        list.set(index, lastVal);
+        map.put(lastVal, index);
+        
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
+    }
+    
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
