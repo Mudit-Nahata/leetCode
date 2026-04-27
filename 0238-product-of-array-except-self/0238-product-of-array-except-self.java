d151d4950d88904eb53bfbdfbc54321c04ad2e0c
Mudit <40465742+Mudit-Nahata@users.noreply.github.com>
2026-04-27 12:07:42 +0530

    Time: 0 ms (100%), Space: 71.9 MB (59.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,17 +1,30 @@
-class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length];
-        answer[0] = 1;
-        for(int i = 1; i < nums.length; i++) {
-            answer[i] = answer[i-1] * nums[i-1];
+public class Solution {
+     static{
+        for(int i=0;i<500;i++){
+        productExceptSelf(new int[1]);
         }
-        int suffix = 1;
-        for(int i = nums.length -1; i >= 0; i--) {
-            answer[i] *= suffix;
-            suffix *= nums[i];
-        }
-
-        return answer;
-
+     }
+public static int[] productExceptSelf(int[] nums) {
+    // if (nums.length==2) {
+    //     int temp=nums[0];
+    //     nums[0]=nums[1];
+    //     nums[1]=temp;
+    //     return nums;
+    // }
+    int [] arr= new int[nums.length];
+    arr[0]=1;
+    for(int i=1;i<arr.length;i++){
+        arr[i]=nums[i-1]*arr[i-1];
     }
+    int temp=1;
+   
+    
+    for(int i=arr.length-1;i>=0;i--){
+      arr[i]=arr[i]*temp;
+      temp*=nums[i];
+    }
+    
+  
+    return arr;
+}
 }
