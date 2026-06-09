c00d8188b7d8c7b49bbf8625883e0ef612757296
Mudit <40465742+Mudit-Nahata@users.noreply.github.com>
2026-06-09 19:38:44 +0530

    Time: 10 ms (100%), Space: 44 MB (60.52%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -1,39 +1,49 @@
 class Solution {
-    
-    Map<TreeNode, Integer> map = new HashMap<>();
-        
+            
     public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
         List<Integer> res = new LinkedList<>();
-        find(root, target);
-        dfs(root, target, K, map.get(root), res);
+        if (K == 0) {
+            res.add(target.val);
+        } else {
+            dfs(res, root, target.val, K ,0);
+        }
         return res;
     }
     
-    // find target node first and store the distance in that path that we could use it later directly
-    private int find(TreeNode root, TreeNode target) {
-        if (root == null) return -1;
-        if (root == target) {
-            map.put(root, 0);
+    private int dfs(List<Integer> res, TreeNode node, int target, int K, int depth) {
+        if (node == null) return 0;
+        
+        if (depth == K) {
+            res.add(node.val);
             return 0;
         }
-        int left = find(root.left, target);
-        if (left >= 0) {
-            map.put(root, left + 1);
+        
+        int left, right;
+        if (node.val == target || depth > 0) {
+            left = dfs(res, node.left, target, K, depth + 1);
+            right = dfs(res, node.right, target, K, depth + 1);
+        } else {
+            left = dfs(res, node.left, target, K, depth);
+            right = dfs(res, node.right, target, K, depth);
+        }
+        
+        if (node.val == target) return 1;
+        
+        if (left == K || right == K) {
+            res.add(node.val);
+            return 0;
+        }
+        
+        if (left > 0) {
+            dfs(res, node.right, target, K, left + 1);
             return left + 1;
         }
-		int right = find(root.right, target);
-		if (right >= 0) {
-            map.put(root, right + 1);
+        
+        if (right > 0) {
+            dfs(res, node.left, target, K, right + 1);
             return right + 1;
         }
-        return -1;
-    }
-    
-    private void dfs(TreeNode root, TreeNode target, int K, int length, List<Integer> res) {
-        if (root == null) return;
-        if (map.containsKey(root)) length = map.get(root);
-        if (length == K) res.add(root.val);
-        dfs(root.left, target, K, length + 1, res);
-        dfs(root.right, target, K, length + 1, res);
+        
+        return 0;
     }
 }
